eabac94e5665c46d69bed29ea6562221917acd8f
Harry <hldodsoniii@gmail.com>
2022-02-19 18:55:28 +0000

    initial commit after running initializr

---
+++ src/main/java/com/dodson/petclinictest/PetClinicTestApplication.java
@@ -0,0 +1,13 @@
+package com.dodson.petclinictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodson/petclinictest/PetClinicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodson.petclinictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
