766e45a0dafc30373e5c289a3ec6de169388e220
Lee <hldodsoniii@gmail.com>
2022-03-09 18:41:22 +0000

    refactored to generate map ids

--- petclinicdata/src/main/java/com/dodson/petclinic/services/map/AbstractMapService.java
+++ petclinicdata/src/main/java/com/dodson/petclinic/services/map/AbstractMapService.java
@@ -1,23 +1,43 @@
 package com.dodson.petclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.dodson.petclinic.model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
     
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() { return new HashSet<>(map.values()); }
 
     T findById(ID id) { return map.get(id); }
 
-    T save(ID id, T object) { 
-        map.put(id, object);
+    T save(T object) { 
+
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Must not be null");
+        }
         return object;
     }
 
     void deleteById(ID id) { map.remove(id);}
     void delete(T object) { map.entrySet().removeIf(entry -> entry.getValue().equals(object)); }
+
+    private Long getNextId() {
+        if(map.keySet().isEmpty()) {
+            return 1L;
+        }
+        else {
+            return Collections.max(map.keySet()) + 1;
+        }
+    }
 }
