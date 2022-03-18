18e8f22a12e81ce7dee3ef9255d79716411e3e0e
Lee <hldodsoniii@gmail.com>
2022-03-18 20:09:15 +0000

    added specialty service map

---
+++ petclinicdata/src/main/java/com/dodson/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,38 @@
+package com.dodson.petclinic.services.map;
+
+import java.util.Set;
+
+import com.dodson.petclinic.model.Specialty;
+import com.dodson.petclinic.services.SpecialtyService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+    
+}
